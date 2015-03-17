690bb4dea3071f65a2daea6777c44fff704fa696
its-micah <mlanier86@gmail.com>
2015-03-17 08:58:35 -0500

    make app work

--- Deprocrastinatoooor/ViewController.m
+++ Deprocrastinatoooor/ViewController.m
@@ -14,6 +14,7 @@
 UITableViewDelegate,
 UITableViewDataSource,
 UIGestureRecognizerDelegate,
+UITextFieldDelegate,
 UIAlertViewDelegate
 >
 
@@ -22,8 +23,9 @@ UIAlertViewDelegate
 @property NSMutableArray *enteredData;
 @property NSMutableArray *enteredColors;
 @property NSString *enteredText;
-//@property (weak, nonatomic) IBOutlet UITableViewCell *cell;
+@property UITableViewCell *selectedCell;
 @property CGPoint pointTapped;
+@property CGPoint pointOfSwipe;
 @property BOOL shouldWeDelete;
 
 @end
@@ -32,28 +34,62 @@ UIAlertViewDelegate
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    self.enteredData = [NSMutableArray arrayWithObjects: nil];
+//    self.enteredColors = [NSMutableArray arrayWithObjects: nil];
     self.enteredData = [NSMutableArray new];
+    self.enteredColors = [NSMutableArray new];
+
     UIGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeRight:)];
     swipeRight.delegate = self;
+    self.tableView.delegate = self;
     self.shouldWeDelete = false;
-    self.enteredColors = [NSMutableArray new];
-    //self.enteredColors = [NSMutableArray arrayWithObjects:[UIColor blackColor], [UIColor blackColor],[UIColor blackColor], nil];
+    self.enteredColors = [NSMutableArray arrayWithObjects:[UIColor blackColor], [UIColor blackColor],[UIColor blackColor], nil];
     [self.tableView addGestureRecognizer:swipeRight];
-    //[self.tableView addGestureRecognizer:swipeRight];
+
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+        NSString *titleItem = [self.enteredData objectAtIndex:sourceIndexPath.row];
+        UIColor *tempColor = [self.enteredColors objectAtIndex:sourceIndexPath.row];
+    [self.enteredData removeObject:titleItem];
+    [self.enteredData insertObject:titleItem atIndex:destinationIndexPath.row];
+    [self.enteredColors removeObject:tempColor];
+    [self.enteredColors insertObject:tempColor atIndex:destinationIndexPath.row];
+
+}
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"WOAH"
+                                                            message:@"Are you sure?"
+                                                           delegate:self
+                                                  cancelButtonTitle:@"Cancel"
+                                                  otherButtonTitles:@"Delete", nil];
+        [alertView show];
+
+    }
+
+    self.shouldWeDelete = false;
+    [self.tableView reloadData];
+
 
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"CellID"];
     self.enteredText = self.textField.text;
-    cell.textLabel.text = [NSString stringWithFormat:@"%@", self.enteredData[indexPath.row]];
+    cell.textLabel.text = [self.enteredData objectAtIndex:indexPath.row];
     cell.textLabel.textColor = [self.enteredColors objectAtIndex:indexPath.row];
-//    if (!(cell.textLabel.textColor == [UIColor blackColor])) {
-//        cell.textLabel.textColor = [self.enteredColors objectAtIndex:indexPath.row];
-//    } else if (cell.textLabel.textColor == [UIColor blackColor]) {
-//        return cell;
-//    }
-
     return cell;
 }
 
@@ -62,71 +98,49 @@ UIAlertViewDelegate
 }
 
 - (IBAction)onAddButtonPressed:(id)sender {
-    self.textField.textColor = [UIColor blackColor];
-    self.enteredText = [NSString stringWithFormat:@"%@", self.textField.text];
-    [self.enteredData addObject:self.textField.text];
-    [self.tableView reloadData];
+    NSString *enteredText = self.textField.text;
+    [self.enteredData addObject:enteredText];
+    [self.enteredColors addObject:[UIColor blackColor]];
     self.textField.text = @"";
     [self.view endEditing:YES];
+    [self.tableView reloadData];
+
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.textLabel.textColor = [UIColor greenColor];
-
+    if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        cell.textLabel.textColor = [UIColor blackColor];
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
 }
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
-}
+
+
+
 
 -(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView setEditing:YES animated:YES];
     return YES;
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"WOAH"
-                                                        message:@"Are you sure?"
-                                                       delegate:self
-                                              cancelButtonTitle:@"Cancel"
-                                              otherButtonTitles:@"Delete", nil];
 
-        [alertView show];
-        //[self.enteredColors removeObjectAtIndex:indexPath.row];
-        [self.enteredData removeObjectAtIndex:indexPath.row];
-
-        self.shouldWeDelete = false;
-}
 
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     if (buttonIndex == 1) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        [self.enteredData removeObject:indexPath];
-        self.shouldWeDelete = true;
+        //self.shouldWeDelete = true;
+        [self.enteredData removeObjectAtIndex:indexPath.row];
+        [self.enteredColors removeObjectAtIndex:indexPath.row];
+
         [self.tableView reloadData];
     }
 }
 
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
-}
-
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
-//    NSString *tempString = [self.enteredData objectAtIndex:sourceIndexPath.row];
-//    UIColor *tempColor = [self.enteredColors objectAtIndex:sourceIndexPath.row];
-    UITableViewCell *tempCell = [tableView cellForRowAtIndexPath:sourceIndexPath];
-    [self.enteredData removeObjectAtIndex:sourceIndexPath.row];
-    [self.enteredData insertObject:tempCell.textLabel.text atIndex:destinationIndexPath.row];
-    [self.enteredColors removeObjectAtIndex:sourceIndexPath.row];
-    [self.enteredColors insertObject:tempCell.textLabel.textColor atIndex:destinationIndexPath.row];
-//    [self.enteredData removeObject:tempString];
-//    [self.enteredData addObject:tempString];
-//    [self.enteredColors removeObject:tempColor];
-//    [self.enteredColors addObject:tempColor];
-    //[self.tableView reloadData];
-}
 
 - (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
     if (self.editing) {
@@ -139,7 +153,6 @@ UIAlertViewDelegate
         [self.tableView setEditing:true animated:true];
         sender.style = UIBarButtonItemStyleDone;
         sender.title = @"Done";
-        [self.view endEditing:YES];
 
     }
 }
